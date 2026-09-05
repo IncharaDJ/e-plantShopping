c568e3cc83c2ec81bd07ce539e678b821c21f99c
IncharaDJ <incharanadeem@gmail.com>
2026-09-05 12:17:21 +0530

    productlist.jsx

--- ProductList.jsx
+++ ProductList.jsx
@@ -1,3 +1,14 @@
-<button onClick={() => addToCart(plant)}>
-  Add to Cart
+import { useDispatch } from "react-redux";
+import { addToCart } from "./CartSlice";
+import { useState } from "react";
+const dispatch = useDispatch();
+const [addedItems, setAddedItems] = useState([]);
+<button
+  onClick={() => {
+    dispatch(addToCart(plant));
+    setAddedItems([...addedItems, plant.id]);
+  }}
+  disabled={addedItems.includes(plant.id)}
+>
+  {addedItems.includes(plant.id) ? "Added to Cart" : "Add to Cart"}
 </button>
